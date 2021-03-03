882bdddfd6fd70c967b8f97ddb7a73e2096fe5bc
MohamedElWaghf <57914752+MohamedElWaghf@users.noreply.github.com>
2021-03-03 12:35:57 +0100

    Update detector.sh

--- bin/detector.sh
+++ bin/detector.sh
@@ -4,7 +4,7 @@ function helper() {
 	reset=$(tput sgr0)
 	echo -e "${red}All commands should be executed at the same directory as README file${reset}"
 	echo -e "\n${green}detector image input_path output_path${reset} : draw bounding boxes around faces within image and detect the emotion, age and gender.${green}Hit Enter button to quit${reset}\n\n\tThe output_path should include the image name and format \n \t Example : 'detector image data/face.jpeg data/output.jpeg' \n
-${green}detector video input_path output_path stride[optional integer,default=1]${reset} : draw bounding boxes around faces every ${green}stride${reset} frames within video and detect the emotion, age and gender.\n\n\tThe output_path should include the video name and format (avi format is privileged) \n \t Example : 'detector video data/china.avi data/output.avi'\n
+${green}detector video input_path output_path stride[optional(integer),default=1]${reset} : draw bounding boxes around faces every ${green}stride${reset} frames within video and detect the emotion, age and gender.\n\n\tThe output_path should include the video name and format (avi format is privileged) \n \t Example : 'detector video data/china.avi data/output.avi'\n
 ${green}detector webcam${reset} : draw bounding boxes around faces within webcam frame and detect the emotion, age and gender.${green}Hit 'q' on the keyboard to quit${reset}\n\n \t Example : 'detector webcam'
 	"
 }
